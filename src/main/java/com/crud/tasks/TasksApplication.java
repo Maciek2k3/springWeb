b1aa312ef54d5841d04bb1ceb9704cd500dc6db5
Maciek2k3 <maciej.palczewski88@gmail.com>
2021-01-28 01:44:32 +0100

    make it better

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -8,16 +8,16 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TasksApplication extends SpringBootServletInitializer {
-
+public class TasksApplication //extends SpringBootServletInitializer {
+{
 	public static void main(String[] args) {
-		TaskContoller taskContoller=new TaskContoller();
+		//TaskContoller taskContoller=new TaskContoller();
 
 		SpringApplication.run(TasksApplication.class, args);
 	}
 
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(TasksApplication.class);
+	//@Override
+	//protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	//	return application.sources(TasksApplication.class);
 	}
-}
+
