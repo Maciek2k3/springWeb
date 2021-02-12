c0c0272d9acdacb8ff109fd6c925e0040eec24e7
Maciek2k3 <maciej.palczewski88@gmail.com>
2021-02-12 09:46:35 +0100

    22.4 Mock test

--- src/main/java/com/crud/tasks/config/CoreConfiguration.java
+++ src/main/java/com/crud/tasks/config/CoreConfiguration.java
@@ -8,6 +8,11 @@ import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -16,6 +21,7 @@ import java.util.Map;
 
 @Configuration
 @Getter
+@EnableSwagger2
 public class CoreConfiguration {
 
     //RestTemplate restTemplate= new RestTemplate();
@@ -28,6 +34,14 @@ public class CoreConfiguration {
         //restTemplate.setMessageConverters(messageConverters);
 
         return new RestTemplate();
+    }
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.crud.tasks.controller"))
+                .paths(PathSelectors.any())
+                .build();
     }
 
 }
