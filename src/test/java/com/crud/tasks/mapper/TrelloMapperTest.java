81adddd48f7696b018377bafaf72b3af20ad661b
Maciek2k3 <maciej.palczewski88@gmail.com>
2021-03-26 19:11:07 +0100

    29.1.FacadeTest

--- src/test/java/com/crud/tasks/mapper/TrelloMapperTest.java
+++ src/test/java/com/crud/tasks/mapper/TrelloMapperTest.java
@@ -6,6 +6,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import java.lang.reflect.Array;
 import java.util.Arrays;
